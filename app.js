db41b55231faa04b3650d8bf93745e5f6d172076
Sushil <grayson.9999@gmail.com>
2025-01-30 19:59:15 +1000

    read data from csv file and generate html list

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 import express from "express";
 import path from "path";
 
+import fs from "fs";
+
+import { makeHtmlString } from "./src/fileMerger.js";
+
 const app = express();
 
 const PORT = 8000;
@@ -16,7 +20,14 @@ app.use(express.static(__dirname + `/public`));
 app.use(express.urlencoded({ extended: true }));
 // home controller
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + `/src/html/index.html`);
+  fs.readFile(fileName, "utf8", (error, data) => {
+    if (error) {
+      res.sendFile(__dirname + `/src/html/index.html`);
+    } else {
+      console.log(data.split("\n"));
+      res.send(makeHtmlString(data.split("\n")));
+    }
+  });
 });
 
 //login controller
@@ -27,11 +38,28 @@ app.get("/login", (req, res) => {
 //registration controller
 app.get("/register", (req, res) => {
   console.log(req.query);
+
   res.sendFile(__dirname + `/src/html/register.html`);
 });
+
+const fileName = "userList.csv";
 app.post("/register", (req, res) => {
   console.log(req.body);
-  res.sendFile(__dirname + `/src/html/register.html`);
+  const { name, email, password } = req.body;
+
+  const str = `${name}, ${email}, ${password} \n`;
+  console.log(str.split(","));
+
+  //create and write file
+
+  fs.appendFile(fileName, str, (error) => {
+    error ? res.send(error.message) : res.redirect("/");
+    // : res.send(
+    //     `<h1 class="alert alert-success">User account created successfully. You may login now!🫠</h1>`
+    //   );
+  });
+
+  // res.sendFile(__dirname + `/src/html/register.html`);
 });
 
 // app.get("/api/v1/get-users", (req, res) => {
